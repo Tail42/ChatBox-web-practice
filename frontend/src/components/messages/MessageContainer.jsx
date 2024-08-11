a236e750dcbda7c108e2048bd6cad200ac85d099
unknown <changtail42@gmail.com>
2024-08-12 04:02:25 +0800

    ordinary push

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import Messages from './Messages'
+import MessageInput from './MessageInput'
+
 
 const MessageContainer = () => {
+  const noChatSelected = true;
   return (
         <div className='md:min-w-[450px] flex flex-col'>
 			<>
@@ -15,5 +19,18 @@ const MessageContainer = () => {
 		</div>
   )
 }
-
 export default MessageContainer
+
+
+const NoChatSelected = () => {
+	const { authUser } = useAuthContext();
+	return (
+		<div className='flex items-center justify-center w-full h-full'>
+			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
+				<p>Welcome 👋 {authUser.fullName} ❄</p>
+				<p>Select a chat to start messaging</p>
+				<TiMessages className='text-3xl md:text-6xl text-center' />
+			</div>
+		</div>
+	);
+};
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import Message from './Message'
 
 const Messages = () => {
   return (
-        <div className={`chat ${chatClassName}`}>
-			<div className='chat-image avatar'>
-				<div className='w-10 rounded-full'>
-					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
-				</div>
-			</div>
-			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
-			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
-		</div>
+	<div className='px-4 flex-1 overflow-auto'>
+		<Message />
+		<Message />
+		<Message />
+		<Message />
+		<Message />
+		<Message />
+		<Message />
+		<Message />
+		<Message />
+		<Message />
+		<Message />
+		<Message />
+		<Message />
+		<Message />
+		<Message />
+	</div>
   )
 }
 
